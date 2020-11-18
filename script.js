7fe61cfdc1dedac847c61bbcb4f4490f2f29dbf0
Le Anh Thang <leanhthang70@gmail.com>
2020-11-18 14:57:39 +0700

    update ui

--- script.js
+++ script.js
@@ -1,14 +1,47 @@
 const video = document.getElementById('inputVideo');
 const canvas = document.getElementById('canvas');
+const inputCMNDFront = document.getElementById('CMNDFront');
 
-Promise.all([
-  faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-  faceapi.nets.ssdMobilenetv1.loadFromUri('/models')
-  // faceapi.nets.faceExpressionNet.loadFromUri('/models')
-]).then(startVideo)
-// ]).then(start)
+function videoOnPlay() {
+  loadFaceapi()
+  // video.play();
+  video.onplay = function() {
+      const displaySize = { width: video.width, height: video.height }
+      faceapi.matchDimensions(canvas, displaySize)
+      console.log("asdas")
+      setInterval(async () => {
+        // let image = await faceapi.fetchImage(avatar.src)
+        const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
+        .withFaceLandmarks().withFaceDescriptors()
+        // .withFaceExpressions()
+        const resizedDetections = faceapi.resizeResults(detections, displaySize)
+
+
+        const labeledFaceDescriptors = await loadLabeledImages()
+        const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6)
+
+        const results = resizedDetections.map(d => faceMatcher.findBestMatch(d.descriptor))
+        results.forEach((result, i) => {
+          canvas.getContext('2d').clearRect(0, 0, video.width, video.height)
+          const box = resizedDetections[i].detection.box
+          if (result._distance <= 5) {
+            $('#ekyc-label-is-confirm').removeClass('text-danger').addClass('text-success');
+          }
+          const drawBox = new faceapi.draw.DrawBox(box, { label: result.toString() })
+          drawBox.draw(canvas)
+        })
+      }, 50)
+  };
+}
+
+function loadFaceapi() {
+  Promise.all([
+    faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
+    faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
+    faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
+    faceapi.nets.ssdMobilenetv1.loadFromUri('/models')
+  ]).then(startVideo);
+}
 
 function startVideo() {
   navigator.getUserMedia(
@@ -18,37 +51,6 @@ function startVideo() {
   )
 }
 
-video.addEventListener('play', () => {
-  // const canvas = faceapi.createCanvasFromMedia(video)
-  // wrapperBox.append(canvas)
-  const displaySize = { width: video.width, height: video.height }
-  faceapi.matchDimensions(canvas, displaySize)
-  console.log("asdas")
-  setInterval(async () => {
-    // let image = await faceapi.fetchImage(avatar.src)
-    const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
-    .withFaceLandmarks().withFaceDescriptors()
-    // .withFaceExpressions()
-    const resizedDetections = faceapi.resizeResults(detections, displaySize)
-
-
-    const labeledFaceDescriptors = await loadLabeledImages()
-    const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6)
-
-    const results = resizedDetections.map(d => faceMatcher.findBestMatch(d.descriptor))
-    results.forEach((result, i) => {
-      canvas.getContext('2d').clearRect(0, 0, video.width, video.height)
-      const box = resizedDetections[i].detection.box
-      if (result._distance <= 5) {
-        console.log("success")
-      }
-      const drawBox = new faceapi.draw.DrawBox(box, { label: result.toString() })
-      drawBox.draw(canvas)
-    })
-
-  }, 50)
-})
-
 function loadLabeledImages() {
   const labels = ['Thang']
   return Promise.all(
@@ -56,8 +58,9 @@ function loadLabeledImages() {
       const descriptions = []
       for (let i = 1; i <= 2; i++) {
 
-        const avatar = await faceapi.fetchImage(`images/thang1.jpg`)
-        const detections = await faceapi.detectSingleFace(avatar)
+        // const avatar = await faceapi.fetchImage(`images/thang1.jpg`)
+        const cmnd = await faceapi.bufferToImage(inputCMNDFront.files[0]);
+        const detections = await faceapi.detectSingleFace(cmnd)
             .withFaceLandmarks()
             .withFaceDescriptor()
         descriptions.push(detections.descriptor)
