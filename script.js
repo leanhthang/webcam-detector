3d3c75b9500f067e2868f1b4d4e1aa7b51fcf239
Thang <leanhthang70@gmail.com>
2020-11-18 11:59:50 +0700

    TMP

--- script.js
+++ script.js
@@ -1,7 +1,4 @@
-const video = document.getElementById('inputVideo');
-const canvas = document.getElementById('canvas');
-const imageUpload = document.getElementById('imageUpload')
-
+// const video = document.getElementById('inputVideo');
 
 Promise.all([
   faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
@@ -9,8 +6,8 @@ Promise.all([
   faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
   faceapi.nets.ssdMobilenetv1.loadFromUri('/models')
   // faceapi.nets.faceExpressionNet.loadFromUri('/models')
-// ]).then(startVideo)
-]).then(start)
+]).then(startVideo)
+// ]).then(start)
 
 function startVideo() {
   navigator.getUserMedia(
@@ -21,58 +18,49 @@ function startVideo() {
 }
 
 video.addEventListener('play', () => {
+  // const canvas = faceapi.createCanvasFromMedia(video)
+  // wrapperBox.append(canvas)
   const displaySize = { width: video.width, height: video.height }
   faceapi.matchDimensions(canvas, displaySize)
+  console.log("asdas")
   setInterval(async () => {
+    // let image = await faceapi.fetchImage(avatar.src)
     const detections = await faceapi.detectAllFaces(video, new faceapi.TinyFaceDetectorOptions())
-    .withFaceLandmarks()
+    .withFaceLandmarks().withFaceDescriptors()
     // .withFaceExpressions()
     const resizedDetections = faceapi.resizeResults(detections, displaySize)
-    canvas.getContext('2d').clearRect(0, 0, canvas.width, canvas.height)
-    faceapi.draw.drawDetections(canvas, resizedDetections)
-
-  }, 100)
-})
 
 
-async function start() {
-  const container = document.createElement('div')
-  container.style.position = 'relative'
-  document.body.append(container)
-  const labeledFaceDescriptors = await loadLabeledImages()
-  const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6)
-  let image
-  let canvas
-  document.body.append('Loaded')
-  imageUpload.addEventListener('change', async () => {
-    if (image) image.remove()
-    if (canvas) canvas.remove()
-    image = await faceapi.bufferToImage(imageUpload.files[0])
-    container.append(image)
-    canvas = faceapi.createCanvasFromMedia(image)
-    container.append(canvas)
-    const displaySize = { width: image.width, height: image.height }
-    faceapi.matchDimensions(canvas, displaySize)
-    const detections = await faceapi.detectAllFaces(image).withFaceLandmarks().withFaceDescriptors()
-    const resizedDetections = faceapi.resizeResults(detections, displaySize)
+    const labeledFaceDescriptors = await loadLabeledImages()
+    const faceMatcher = new faceapi.FaceMatcher(labeledFaceDescriptors, 0.6)
+
     const results = resizedDetections.map(d => faceMatcher.findBestMatch(d.descriptor))
     results.forEach((result, i) => {
+      canvas.getContext('2d').clearRect(0, 0, video.width, video.height)
       const box = resizedDetections[i].detection.box
+      if (result._distance <= 5) {
+        console.log("success")
+      }
       const drawBox = new faceapi.draw.DrawBox(box, { label: result.toString() })
       drawBox.draw(canvas)
     })
-  })
-}
+
+  }, 50)
+})
 
 function loadLabeledImages() {
-  const labels = ['Black Widow', 'Captain America', 'Captain Marvel', 'Hawkeye', 'Jim Rhodes', 'Thor', 'Tony Stark']
+  const labels = ['Thang']
   return Promise.all(
     labels.map(async label => {
       const descriptions = []
       for (let i = 1; i <= 2; i++) {
-        const img = await faceapi.fetchImage(`https://raw.githubusercontent.com/WebDevSimplified/Face-Recognition-JavaScript/master/labeled_images/${label}/${i}.jpg`)
-        const detections = await faceapi.detectSingleFace(img).withFaceLandmarks().withFaceDescriptor()
+
+        const avatar = await faceapi.fetchImage(`images/thang1.jpg`)
+        const detections = await faceapi.detectSingleFace(avatar)
+            .withFaceLandmarks()
+            .withFaceDescriptor()
         descriptions.push(detections.descriptor)
+        console.log(descriptions)
       }
 
       return new faceapi.LabeledFaceDescriptors(label, descriptions)
