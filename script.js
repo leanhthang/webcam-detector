9f78fca19e23889487f15d7354baaaa8e70151ee
Le Anh Thang <leanhthang70@gmail.com>
2020-11-19 09:57:38 +0700

    update

--- script.js
+++ script.js
@@ -64,7 +64,7 @@ function loadFaceapi() {
   ]).then(startVideo);
 }
 function startVideo() {
-  navigator.getUserMedia(
+  navigator.mediaDevices.getUserMedia(
     { video: {} },
     stream => video.srcObject = stream,
     err => console.error(err)
